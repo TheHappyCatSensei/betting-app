ba4846f6d8bc4cb66c803ecb1e0f02cedd19c771
TheHappyCat <happycat.dev@gmail.com>
2021-07-08 16:02:59 +0000

    git

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Betting App',
       theme: ThemeData(
         primarySwatch: Colors.green,
