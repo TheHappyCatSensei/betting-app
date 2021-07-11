396c46612b441cb5056759aa1abc53e4fdb06986
TheHappyCat <happycat.dev@gmail.com>
2021-07-11 02:29:34 +0000

    Added appBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Betting App'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            size: 30,
+          ),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
